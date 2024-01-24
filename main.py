5d246ae63f5c8b66da754594626cc508348911e8
Hemanth <128178031+hemanth090@users.noreply.github.com>
2024-01-24 09:12:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,43 @@
 import streamlit as st
 import yfinance as yf
 from datetime import datetime, timedelta
+import altair as alt
+import pandas as pd
 
-# Function to fetch stock data
 def fetch_stock_data(ticker, start_date, end_date):
     data = yf.download(ticker, start=start_date, end=end_date)["Close"]
     return data
 
-# Streamlit app layout
 def main():
     st.title("Stock Analysis")
 
-    # Input for ticker symbol
     ticker_symbol = st.text_input("Enter the ticker symbol (e.g., AAPL for Apple):", "AAPL")
 
-    # Input for date range
     start_date = st.date_input("Select start date", datetime.now() - timedelta(days=365))
     end_date = st.date_input("Select end date", datetime.now())
-
-    # Fetch and display data button
     if st.button("Fetch and Analyze Data"):
         with st.spinner('Fetching data...'):
             try:
-                # Fetch data for the selected date range
                 data_custom_range = fetch_stock_data(ticker_symbol, start_date, end_date)
 
-                # Display the data
-                st.write(f"Stock Data for {ticker_symbol} - Custom Date Range:")
-                st.dataframe(data_custom_range)
+                chart_data = pd.DataFrame({"Date": data_custom_range.index, "Stock Price": data_custom_range.values})
+                chart = alt.Chart(chart_data).mark_line().encode(
+                    x='Date:T',
+                    y='Stock Price:Q',
+                    tooltip=['Date:T', 'Stock Price:Q']
+                ).properties(width=800, height=400)
+
+                col1, col2 = st.columns([2, 1])
+
+                # Display chart 
+                col1.altair_chart(chart, use_container_width=True)
+
+                # Display the data in a dataframe in the second column
+                col2.write("Stock Prices Data:")
+                col2.dataframe(data_custom_range)
 
             except Exception as e:
                 st.error(f"Error: {e}. Please check the provided ticker symbol.")
-
 
 if __name__ == "__main__":
     main()
